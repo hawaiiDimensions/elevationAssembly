974a9d176110446fef3432aea0ef883c582150ff
Jun Ying Lim <junyinglim@gmail.com>
2018-04-30 13:34:57 -0700

    Generate rarefied datasets for ARF locus

--- generateRarefiedMatrices.R
+++ generateRarefiedMatrices.R
@@ -52,7 +52,7 @@ test <- ddply(.data = dataARFmelt, .var = .(SizeCategory, Site_ID), .fun = sumRe
 test2 <- test[test$totalReads < test$rarefiedReads,]
 dim(test2)
 
-# Rarefy data
+# Rarefy datasets
 # Samples where there are fewer reads than expected will be left unrarefied (only 17 site by size groupings)
 nreps <- 100
 arf_rarefied <- list()
@@ -67,71 +67,28 @@ for(i in 1:nreps){
 }
 
 # Collapse data into OTUs
-collapseOTUs <- function()
+collapseOTUs <- function(df){
+  # Collapses rarefied read abundance data frame by values in target column
+  # Args:
+  #   df = data.frame object
+  #   collapse.by = string, name of column to collapse by
+  # Returns:
+  #   df = data.frame
+  readAbund <- sum(df$rarefiedReadAbund)
+  return(data.frame(readAbund))
+}
 
+collapseOTUsList <- function(x, collapse.by){
+  # Wrapper function to iterate ddply function across each rarefied data frame using lapply
+  ddply(.data = x, .fun = collapseOTUs, .variables = c("Site_ID", collapse.by), .progress = "text")
+}
 
+arfSpeciesID <- lapply(X = arf_rarefied, FUN = collapseOTUsList, collapse.by = "Species_ID")
+arfZOTUID <- lapply(X = arf_rarefied, FUN = collapseOTUsList, collapse.by = "zOTU_ID")
+arfOTUID <- lapply(X = arf_rarefied, FUN = collapseOTUsList, collapse.by = "OTU_ID")
+head(arf_rarefied[[1]])
 
-
-  
-  
-  
-  
 ## EXPORT RAREFIED MATRICES
-saveRDS()
-
-
-
-# Collapse data by OTUs and zOTUs
-temp <- str_split_fixed(dataARF$X.OTU.ID, n = 3, pattern = "_")
-dataARF$SpeciesID <- temp[,1]
-dataARF$ZOTU_ID <- temp[,2]
-dataARF$OTU_ID <- temp[,3]
-
-targetCol<- names(dataARF)[grep(names(dataARF), pattern = "X[1-9]")]
-
-collapseData <- function(df){
-  return(colSums(df[targetCol]))
-}
-
-otuARF <- ddply(.data = dataARF, .fun = collapseData, .variables = .(OTU_ID))
-zotuARF <- ddply(.data = dataARF, .fun = collapseData, .variables = .(OTU_ID))
-
-# Collapse the OTU and ZOTU tables by site, across size cateogries
-zotuARF_melt <- melt(zotuARF, id.vars = "OTU_ID")
-temp <- str_split_fixed(zotuARF_melt$variable, n = 3, pattern = "_")
-zotuARF_melt$site_ID <- temp[,1]
-zotuARF_melt$siteCategory <- temp[,2]
-
-collapseSites <- function(df){
-  return(data.frame("nreads" = sum(df$value)))
-}
-
-zotuARF_site <- ddply(.data = zotuARF_melt, .fun = collapseSites, .variables = .(site_ID, OTU_ID), progress = "text")
-zotuARF_site_mat <- acast(zotuARF_site, site_ID ~ OTU_ID, value.var = "nreads")
-
-rownames(zotuARF_site_mat) <- gsub(rownames(zotuARF_site_mat), pattern = "X", replacement = "")
-
-# Calculate number of OTUs per sample
-zotuARF_site_mat_pa <- ifelse(zotuARF_site_mat >0, 1, 0) # change to presence/absence
-otuSite <- rowSums(zotuARF_site_mat_pa) # calculate no. of OTUs per site
-
-readRandomize <- otuSite*100
-
-sample(zotuARF_site_mat)
-
-randomizeMatrix(zotuARF_site_mat, )
-sample(zotuARF_site_mat)
-
-x <- rrarefy(zotuARF_site_mat[1,], readRandomize[1])
-x[1:5]
-zotuARF_site_mat[1,1:5]
-rowSums(zotuARF_site_mat_pa)
-rowSums(x)
-
-
-# Randomize otuARF
-
-
-# Analyze OTUs
-head(otuARF_melt)
-
+saveRDS(arfSpeciesID, file = file.path(data.dir,"arfSpeciesIDdata.rds"))
+saveRDS(arfZOTUID, file = file.path(data.dir,"arfZOTUIDdata.rds"))
+saveRDS(arfOTUID, file = file.path(data.dir,"arfOTUIDdata.rds"))
