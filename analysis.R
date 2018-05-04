dddcad780466db7c110d3887b4cd2a076f0fd5ca
Jun Ying Lim <junyinglim@gmail.com>
2018-05-03 22:38:05 -0700

    Visualize site climate

--- analysis.R
+++ analysis.R
@@ -9,6 +9,7 @@ library(reshape2)
 library(picante)
 library(betapart)
 library(geosphere)
+library(vegan)
 
 ## IMPORT DATA
 main.dir <- "~/Dropbox/Projects/2017/hawaiiCommunityAssembly/"
@@ -29,6 +30,20 @@ siteData <- read.csv(file.path(data.dir, "clim.final.csv"))
 rownames(siteData) <- siteData$site.id
 climDist <- dist(siteData[c("PC1", "PC2", "PC3")])
 
+climPCA <- prcomp(siteData[c(paste0("BIO", 1:19))], center = TRUE, scale. = TRUE)
+head(climPCA$x)
+head(climPCA$rotation)
+climPCA$sdev / sum(climPCA$sdev) * 100
+which.max(as.data.frame(climPCA$rotation)$PC1)
+as.data.frame(climPCA$rotation)$PC2
+
+climPCAcoord <- as.data.frame(climPCA$x)
+climPCAcoord$Site_ID <- rownames(climPCAcoord)
+library(ggplot2)
+ggplot(data = climPCAcoord) + geom_point(aes(y = PC1, x = PC2)) + geom_text_repel(aes(y = PC1, x = PC2, label = Site_ID))
+library(ggrepel)
+head(climPCAcoord)
+
 ## CALCULATE GEOGRAPHIC DISTANCE BETWEEN SITES
 siteData$latitude, siteData$longitude
 geogDist <- dist(siteData)
@@ -44,9 +59,20 @@ testData_betadist <- testData_beta$beta.sor
 
 ## MANTEL TESTS
 climDist <- matchDist(testData_betadist, climDist)
-geogDist <- matchDist(testData_betadist)
+#geogDist <- matchDist(testData_betadist)
 mantel(climDist, testData_betadist)
 
+## Mantel tests;
+# * climate distance vs. turnover
+# * geographic distance vs. turnover
+
+# Plot abundance against PC1, PC2 (for groups that are found in more than 5 sites, in each transect)
+# Plot abundance against PC1, PC2 for groups that are found in both sites, highlight by site
+# Find the mean? Niche distance? Schoener's D
+# Fit a truncated normal distribution? 
+
+# Remove collembolla
+# 
 
 
 
