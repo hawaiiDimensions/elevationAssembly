7724c09b6261e18fdde84f7c519684ae71850836
Jun Ying Lim <junyinglim@gmail.com>
2019-04-12 16:20:42 +0200

    Calculate hypervolume and climatic ranges using quantiles

--- analysis.R
+++ analysis.R
@@ -41,6 +41,7 @@ OTU_df2 <- merge(x = OTU_df, y = taxonData[c("OTU_ID", "V4")], by = "OTU_ID", al
 OTU_df2 <- OTU_df2[!duplicated(OTU_df2),]
 OTU_df3 <- merge(x = OTU_df2, y = siteData[c("site.id", "rf_ann", "t_ann", "site")], by = "site.id")
 
+## Calculate hypervolumes ==== 
 library(hypervolume)
 # Determine suitability (i.e., sample size) for hypervolume analysis
 determineSuitability <- function(x){
@@ -66,9 +67,61 @@ OTU_hypervols <- dlply(.data = subset(OTU_df_suit2, suitable == "yes" & rarefied
 saveRDS(OTU_hypervols, file.path(res.dir, "OTU_hypervol.rds"))
 
 library(plyr)
+OTU_hypervols <- readRDS(file.path(res.dir, "OTU_hypervol.rds"))
 OTU_hypervol_df <- ldply(.data = OTU_hypervols, .fun = function(x){data.frame( hypervol = x$hypervol@Volume, site = x$site, OTU_ID = x$OTU_ID, Order = x$Order) } )
 
+OTU_hypervol_df2 <- dcast(OTU_hypervol_df, formula = OTU_ID ~ site, value.var = "hypervol")
+ggplot(OTU_hypervol_df2) + geom_point(aes(y = Laupahoehoe, x = Stainback)) + geom_abline(slope = 1, intercept = 0)
+
+
 ggplot(data = OTU_hypervol_df) + geom_boxplot(aes(y = hypervol, x = site, fill = Order))
+ggplot(data = OTU_hypervol_df) + geom_boxplot(aes(y = hypervol, x = site, fill = Order))
+
+# Calculate quantiles ======
+with(OTU_df3, tapply(rf_ann, INDEX = site, FUN = range))
+with(OTU_df3, tapply(t_ann, INDEX = site, FUN = range))
+
+findLimits <- function(x){
+  rf_v <- rep(x$rf_ann, x$rarefiedReadAbund)
+  temp_v <- rep(x$t_ann, x$rarefiedReadAbund)
+  rf_q <- quantile(rf_v, probs = c(0.025, 0.5, 0.975))
+  temp_q <- quantile(temp_v, probs = c(0.025, 0.5, 0.975))
+  temp_lower <- temp_q[1] 
+  temp_median <- temp_q[2]
+  temp_upper <- temp_q[3]
+  rf_lower <- rf_q[1]
+  rf_median <- rf_q[2]
+  rf_upper <- rf_q[3]
+  data.frame(temp_lower, temp_upper, temp_median, rf_lower, rf_upper, rf_median, order = x$V4[1])
+}
+test <- ddply(.data = subset(OTU_df_suit2, suitable == "yes"), .variables = .(site, OTU_ID), .fun = findLimits)
+
+
+subset(OTU_df_suit2, site == "Laupahoehoe" & OTU_ID == "OTU300" & rarefiedReadAbund >0)
+subset(OTU_df_suit2, site == "Laupahoehoe" & OTU_ID == "OTU74" & rarefiedReadAbund >0)
+
+test$rf_range <- test$rf_upper - test$rf_lower
+test$rf_site_upperlimit <- ifelse(test$site == "Laupahoehoe", ifelse(test$rf_upper == 4402.763, 1, 0), ifelse(test$rf_upper == 6388.352, 1, 0))
+test$rf_site_lowerlimit <- ifelse(test$site == "Laupahoehoe", ifelse(test$rf_lower == 2840.776, 1, 0), ifelse(test$rf_lower == 2480.524, 1, 0))
+
+test$temp_range <- test$temp_upper - test$temp_lower
+test$temp_site_upperlimit <- ifelse(test$site == "Laupahoehoe", ifelse(test$temp_upper == 17.89003, 1, 0), ifelse(test$temp_upper == 18.16707, 1, 0))
+test$temp_site_lowerlimit <- ifelse(test$site == "Laupahoehoe", ifelse(test$temp_lower == 13.54576, 1, 0), ifelse(test$temp_lower == 12.28192, 1, 0))
+
+ggplot(data = subset(test, !(temp_site_lowerlimit==1 | temp_site_upperlimit == 1))) + 
+  geom_boxplot(aes(y = temp_range, x = site, fill = order))
+library(lmerTest)
+mod <- lmer(temp_range ~ site + (1|order), 
+     data = subset(test, !(temp_site_lowerlimit==1 | temp_site_upperlimit == 1)))
+MuMIn::r.squaredGLMM(mod)
+
+summary(lm(temp_range ~ site, 
+   data = subset(test, !(temp_site_lowerlimit==1 | temp_site_upperlimit == 1))))
+
+y <- dcast( subset(test, !(temp_site_lowerlimit==1 | temp_site_upperlimit == 1)), formula = OTU_ID ~ site, value.var = "temp_median")
+taxonDataOTU <- taxonData[c("OTU_ID", "V4")][!duplicated(taxonData[c("OTU_ID", "V4")]),]
+y2 <- merge(x = y, y = taxonDataOTU, by = "OTU_ID", all = FALSE)
+ggplot(aes(y = Laupahoehoe, x = Stainback),data = y2) + geom_point(aes(color = V4)) + geom_abline(slope = 1, intercept = 0) + geom_smooth(method = "lm")
 
 
 # The weights do not deal with zero weights as expected...
